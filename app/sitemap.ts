26098fdba0b4e676ab28ef4d3c31ff0921d38a15
J. Vladimir Aglr. <vlad.aguilar.o@gmail.com>
2026-09-04 15:58:41 -0600

    Add static export for sitemap
    Add dynamic export to enforce static generation.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/metadata";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = siteConfig.url;
 
