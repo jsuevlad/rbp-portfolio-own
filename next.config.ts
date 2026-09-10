3b09a76b04c7b2cb6772f82252628bba5dbea85c
J. Vladimir Aglr. <vlad.aguilar.o@gmail.com>
2026-09-10 17:55:48 -0600

    Add basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "",
+  assetPrefix: "",
   productionBrowserSourceMaps: false,
   images: {
     unoptimized: true,
